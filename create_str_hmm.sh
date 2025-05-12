bb92aeb3b6a7cdcd1d2f1658c1e247d6930428ed
giacomo-timelli <timelli.giacomo@gmail.com>
2025-05-12 11:48:25 +0200

    Update create_str_hmm.sh

--- create_str_hmm.sh
+++ create_str_hmm.sh
@@ -1,7 +1,49 @@
-# Convert the PDBefold alignment to FASTA format (uppercase, one-line), removing 'PDB:' prefix
-awk '{if (substr($1,1,1)==">") {print "\n"toupper($1)} else {printf "%s",toupper($1)}}' pdb_kunitz_clstr_rep.ali | sed s/PDB:// | tail -n +2 > pdb_kunitz_clstr_rep_ali_str.fasta 
-#is the file aligned structurally and with the most representatives. Contains the training set to generate the HMM model.
+#!/usr/bin/bash
+
+# Input file from PDBefold
+input="pdb_kunitz_clstr_rep.ali"
+
+# Output file in Stockholm format
+output="pdb_kunitz_clstr_rep_ali_str.sth"
+
+# Add Stockholm header
+echo "# STOCKHOLM 1.0" > "$output"
+
+# Convert alignment to Stockholm without introducing empty lines
+awk '
+BEGIN {
+    seqname = ""
+    seq = ""
+}
+{
+    gsub(/\r/, "")  # remove carriage return (if present)
+    if (substr($1, 1, 1) == ">") {
+        if (seqname != "") {
+            print seqname, seq >> output
+        }
+        seqname = toupper(substr($1, 2))
+        gsub(/^PDB:/, "", seqname)
+        gsub(":", "_", seqname)
+        seq = ""
+    } else {
+        seq = seq toupper($0)
+    }
+}
+END {
+    if (seqname != "") {
+        print seqname, seq >> output
+    }
+}
+' output="$output" "$input"
+
+# Add end-of-file tag for Stockholm
+echo "//" >> "$output"
+
+
+
+
 
 # Build the HMM model from the aligned sequences
-hmmbuild pdb_kunitz_clstr_rep_ali_str.hmm pdb_kunitz_clstr_rep_ali_str.fasta
+hmmbuild pdb_kunitz_clstr_rep_ali_str.hmm pdb_kunitz_clstr_rep_ali_str.sth
+
 
